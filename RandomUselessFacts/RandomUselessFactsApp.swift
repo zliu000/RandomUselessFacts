ddffac6094cb738ffaaec5306893a0ce0c2a7b57
BT_C09_12 <mobileappdev@bths.edu>
2026-04-27 13:31:34 -0400

    Initial Commit

---
+++ RandomUselessFacts/RandomUselessFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUselessFactsApp.swift
+//  RandomUselessFacts
+//
+//  Created by Student on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct RandomUselessFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
