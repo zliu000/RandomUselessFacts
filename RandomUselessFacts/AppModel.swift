36d3724cd0188da8d766a5663dae4466d77fdc9b
BT_C09_12 <mobileappdev@bths.edu>
2026-04-29 13:53:52 -0400

    Set up MVVM (check comments)

--- RandomUselessFacts/AppModel.swift
+++ RandomUselessFacts/AppModel.swift
@@ -3,5 +3,19 @@ import SwiftUI
 @Observable
 class AppModel {
     private(set) var swipes = 0
+    private(set) var favoritedFacts: [UselessFact] = []
     
+    func getFavoritedFacts() -> [UselessFact]{
+        return favoritedFacts
+    }
+    
+    // When displaying the new randomly fetched fact
+    // Check if it's already been favorited or not
+    func addFavoritedFacts(favorite: UselessFact) {
+        favoritedFacts.append(favorite)
+    }
+    
+    func removeFavoritedFacts(idx: Int) {
+        favoritedFacts.remove(at: idx)
+    }
 }
