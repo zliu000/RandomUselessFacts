b073164ca52fee13141b56c7673fd3cf26d5678a
BT_C09_12 <mobileappdev@bths.edu>
2026-05-07 13:29:49 -0400

    Moved FactDisplay to own file

--- RandomUselessFacts/NewTinderView.swift
+++ RandomUselessFacts/NewTinderView.swift
@@ -52,33 +52,6 @@ struct NewTinderView: View {
     }
 }
 
-struct FactDisplay: View {
-    var fact: String
-    
-    var body: some View {
-        VStack(
-            alignment: .center
-        ){
-            Text(fact)
-                .font(.title)
-                .lineSpacing(10)
-                .foregroundStyle(
-                    .orange
-                )
-        }
-        .padding()
-        .background(
-            LinearGradient(gradient: Gradient(colors: [
-                Color(red: 0.631, green: 1, blue: 0.808), // #a1ffce
-                Color(red: 0.98, green: 1, blue: 0.82) // #faffd1
-                ]), startPoint: .top, endPoint: .bottom)
-        )
-        .clipShape(
-            RoundedRectangle(cornerRadius: 15.0)
-        )
-        .shadow(color: .gray, radius: 3, x: 5, y: 5)
-    }
-}
 
 struct DetailDisplay: View {
     var source: String
