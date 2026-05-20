d6aa19d4347eb35f6372a839e6a4df7ed8b3678b
BT_C09_15 <mobileappdev@bths.edu>
2026-05-20 11:01:13 -0400

    Fatal error due to networkClient resolved

--- RandomUselessFacts/RandomUselessFactsApp.swift
+++ RandomUselessFacts/RandomUselessFactsApp.swift
@@ -3,11 +3,13 @@ import SwiftUI
 @main
 struct RandomUselessFactsApp: App {
     @State private var appModel = AppModel()
+    @State private var networkClient = NetworkClient()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(appModel)
+                .environment(networkClient)
         }
     }
 }
